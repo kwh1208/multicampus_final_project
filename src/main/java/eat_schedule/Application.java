77f74f91317e72298a8f89388177459c7b7a101e
kwh <kwh871005@gmail.com>
2023-04-22 15:47:25 +0900

    [Feat] recover

--- src/main/java/eat_schedule/Application.java
+++ src/main/java/eat_schedule/Application.java
@@ -1,11 +1,12 @@
 package eat_schedule;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
